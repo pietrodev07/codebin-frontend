2a2e51aa8ebb4c0d54d4962b1e77e117303ae9ef
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-22 16:20:16 +0200

    feat(services-snippets-api): add create-snippet service

--- src/services/snippets/snippets.api.ts
+++ src/services/snippets/snippets.api.ts
@@ -1,6 +1,6 @@
 import { Response } from "@/types";
-import { Snippet } from "@/types/snippet";
 import { api, SNIPPETS_URL, generateUrl } from "@/api";
+import { CreateSnippet, Snippet } from "@/types/snippet";
 
 export const getSnippets = async () => {
   const url = generateUrl(SNIPPETS_URL, "/private");
@@ -18,4 +18,10 @@ export const getPublicSnippet = async (id: string) => {
   const url = generateUrl(SNIPPETS_URL, `/${id}`);
   const res = await api.get(url);
   return res as Response<Snippet>;
+};
+
+export const createSnippet = async (snippet: CreateSnippet) => {
+  const url = generateUrl(SNIPPETS_URL, "/private");
+  const res = await api.post(url, snippet);
+  return res as Response<null>;
 };
--- src/services/snippets/snippets.hook.ts
+++ src/services/snippets/snippets.hook.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 
+import { renderToast } from "@/utils/toast";
 import { SnippetsState } from "./snippets.model";
 import * as snippetsService from "./snippets.api";
 
@@ -21,7 +22,10 @@ export const useSnippets = create<SnippetsState>((set) => {
       const res = await snippetsService.getPublicSnippet(id);
       res.success && set({ snippet: res.data });
     },
-    createSnippet: async () => {},
+    createSnippet: async (snippet) => {
+      const res = await snippetsService.createSnippet(snippet);
+      renderToast(res.success, res.message);
+    },
     editSnippet: async () => {},
     deleteSnippet: async () => {},
   };
