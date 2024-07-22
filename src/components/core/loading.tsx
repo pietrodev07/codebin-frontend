e7bb141b3321a0de3a2f66645e2fb3d6b593e0bd
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-22 16:29:34 +0200

    feat(components-core): add loading component

---
+++ src/components/core/loading.tsx
@@ -0,0 +1,10 @@
+export const Loading = () => {
+  return (
+    <div className="spinner-wave mb-auto ml-auto mr-auto mt-auto">
+      <div className="spinner-wave-dot"></div>
+      <div className="spinner-wave-dot"></div>
+      <div className="spinner-wave-dot"></div>
+      <div className="spinner-wave-dot"></div>
+    </div>
+  );
+};
