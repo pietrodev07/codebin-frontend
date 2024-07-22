4af556d53323f63d697954bfe8bfd7b7101065a0
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-22 15:18:57 +0200

    feat(utils-formatter): add format-date util method

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -5,3 +5,19 @@ export const camelToNormal = (str: string) => {
     .replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
     .trim();
 };
+
+export const formatDate = (isoDate: string) => {
+  const date = new Date(isoDate);
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  };
+
+  return date.toLocaleDateString("en-US", options);
+};
