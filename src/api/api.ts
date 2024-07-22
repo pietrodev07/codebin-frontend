7272dc09bbb64196094bf6931f1359f1cb007510
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-22 15:28:52 +0200

    feat(api-wrapper): add post method

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,3 +6,14 @@ export const get = async (url: string) => {
 
   return await response.json();
 };
+
+export const post = async (url: string, body: object) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  return await response.json();
+};
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import { get } from "./api";
+import { get, post } from "./api";
 import { SearchParams } from "@/types";
 
 export const SERVER_URL = import.meta.env.VITE_API_BASE_URL;
@@ -27,4 +27,4 @@ export const generateUrl = (
   return compelteUrl;
 };
 
-export const api = { get };
+export const api = { get, post };
