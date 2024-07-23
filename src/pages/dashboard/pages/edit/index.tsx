579dabc989d0238efb136dab0c48924095089968
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-23 12:23:28 +0200

    feat(dashboard-edit-page): add necessary components into the page

--- src/pages/dashboard/pages/edit/index.tsx
+++ src/pages/dashboard/pages/edit/index.tsx
@@ -1,3 +1,47 @@
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { LanguageName } from "@uiw/codemirror-extensions-langs";
+
+import { useEditSnippet } from "./hooks/use-edit-snippet";
+import { SnippetForm } from "../../components/snippet-form";
+import { ConfirModal } from "../../components/confirm-modal";
+import { useSnippets } from "@/services/snippets/snippets.hook";
+import { CodeEditor } from "@/pages/dashboard/components/code-editor";
+
 export const DashboardEditPage = () => {
-  return <h1 className="title">Dashboard Edit Page</h1>;
+  const { actions, validators, isDirty, snippet } = useEditSnippet();
+  const { getPrivateSnippet, modal, toggleModal } = useSnippets();
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    getPrivateSnippet(id!);
+  }, [getPrivateSnippet, id]);
+
+  return (
+    <section className="flex flex-1 gap-5 max-md:flex-col">
+      <CodeEditor
+        onChange={actions.handleEditorChange}
+        code={snippet.code}
+        language={snippet.language as LanguageName}
+      />
+
+      <SnippetForm
+        validators={validators}
+        actions={actions}
+        isDirty={isDirty}
+        snippet={snippet}
+      />
+
+      <ConfirModal
+        isOpen={modal.isOpen}
+        onConfirm={() => {
+          toggleModal();
+          navigate("/dashboard");
+        }}
+        onCancel={toggleModal}
+        title="Are you sure to cancel the editation?"
+      />
+    </section>
+  );
 };
