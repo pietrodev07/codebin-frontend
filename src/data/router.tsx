52e54ae00a2cdbec7f34002c8bb97d2f7dad22e3
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-23 09:41:17 +0200

    feat(router-pages): add home page

--- src/data/router.tsx
+++ src/data/router.tsx
@@ -1,5 +1,12 @@
+import { HomePage } from "@/pages";
 import { createBrowserRouter } from "react-router-dom";
 
 type Router = ReturnType<typeof createBrowserRouter>;
 
-export const router: Router = createBrowserRouter([]);
+export const router: Router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    element: <HomePage />,
+  },
+]);
