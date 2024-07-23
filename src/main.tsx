ee5d58fdee1d228f074afb6bb648f4afbafe72a0
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-23 11:50:19 +0200

    feat(app): add theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import { App } from "./app";
+import { ThemeProvider } from "next-themes";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
+);
