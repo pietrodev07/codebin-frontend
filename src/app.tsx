3f8e7460287400233a055ba1c4e335a7f31ce5bf
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-23 12:29:38 +0200

    feat(app): add me fetching in the app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,25 @@
 import { Toaster } from "sonner";
+import { useEffect } from "react";
 import { useTheme } from "next-themes";
 import { RouterProvider } from "react-router-dom";
 
 import { Theme } from "@/types";
 import { router } from "@/data/router";
 import { toastOptions } from "@/utils/toast";
+import { useAuth } from "@/services/auth/auth.hook";
+import { Loading } from "./components/core/loading";
 
 export const App = () => {
+  const { isLoading, me } = useAuth();
   const { theme } = useTheme();
+
+  useEffect(() => {
+    me();
+  }, [me]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <>
