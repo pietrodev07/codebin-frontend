f0b53a50bb19fd2a32e4180ed2d7414f3582e0ba
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-23 11:38:43 +0200

    feat(verify-page): add verify button into the page

--- src/pages/auth/verify/index.tsx
+++ src/pages/auth/verify/index.tsx
@@ -1,12 +1,26 @@
+import { useVerify } from "./hooks/use-verify";
 import { AuthLayout } from "@/components/layout/auth-layout";
 
 export const VerifyAccountPage = () => {
+  const { actions, userEmail } = useVerify();
+
   return (
     <AuthLayout
       navbar={{ loginBtn: true }}
       className="flex flex-col items-center justify-center gap-5"
     >
-      <h1 className="title">Verify Account Page</h1>
+      <h1 className="text-2xl font-bold text-content2">Hello [{userEmail}]</h1>
+
+      <h2 className="text-center text-2xl font-bold text-content2">
+        Click the button to verify your account!
+      </h2>
+
+      <button
+        onClick={actions.handleVerify}
+        className="btn btn-solid-primary border border-border"
+      >
+        Verify
+      </button>
     </AuthLayout>
   );
 };
